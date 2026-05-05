98c26512d688dc4b900a5ca37b7e1297e7a4dd5c
Glenn <gpiludu@gmail.com>
2026-05-05 02:08:00 +0000

    Added more generic labels to land_project_sites data Updated Node version and some corepack settings in the Dockerfile because dev containers stopped working

--- src/components/Map/Layer.tsx
+++ src/components/Map/Layer.tsx
@@ -13,6 +13,16 @@ interface LayerProps {
   collection: FeatureCollection
 }
 
+/* 
+  TODO: We need to define some generic params that will be returned in a FeatureCollection's
+  properties. Then we can actually use the type capabilities in Typescript.
+  Some ideas:
+    - identifier
+    - measurement
+    - unitOfMeasure
+    - partners (partnerships?) referring to Tribes, AdvocacyGroups, etc. with whom we work
+    - ??? some ability to have adhoc fields
+*/
 
 /* Given a collection, returns an array of <GeoJSON> leaflet components for non-null features. */
 export function Layer({ collection }: LayerProps): JSX.Element[] {
@@ -32,13 +42,13 @@ export function Layer({ collection }: LayerProps): JSX.Element[] {
         // Popup is a clickable element.
         <Popup>
           <p>{feature.properties?.identifier ?? ''}</p>
-          <p>Tribe(s) : {feature.properties?.tribes.join(', ')}</p>
-          <p>Current Acreage: {feature.properties?.acreage}</p>
+          <p>Land Partner(s): {feature.properties?.partners.join(', ')}</p>
+          <p>Current Acreage: {feature.properties?.measurement}</p>
           <p>Restoration Services: {feature.properties?.restorationServices.join(', ')}</p>
         </Popup>
         <Circle
           center={[coordinates[1], coordinates[0]]}
-          radius={Math.sqrt(feature.properties?.acreage * 4046.86)} // 1 acre = 4046.86 m^2
+          radius={Math.sqrt(feature.properties?.measurement * 4046.86)} // 1 acre = 4046.86 m^2
         />
         </GeoJSON>
        )  
